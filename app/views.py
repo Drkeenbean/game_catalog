082f4adbad3e21aed25d871997ad6b76af03b866
David Varner <david.r.varner@gmail.com>
2016-01-13 16:18:42 -0500

    Add google login and logout to protect items and item creation:

--- app/views.py
+++ app/views.py
@@ -1,11 +1,21 @@
 from app import app
-from flask import render_template, url_for, request, redirect, jsonify
+from flask import render_template, url_for, \
+    request, redirect, jsonify, make_response
 from flask import session as login_session
-from models import Base, Item, Genre, Platform
+from models import Base, Item, Genre, Platform, User
 from sqlalchemy import create_engine, desc
 from sqlalchemy.orm import sessionmaker
 import random
 import string
+import json
+import requests
+
+# Google sign in imports
+from googleapiclient import discovery
+import httplib2
+from oauth2client import client
+
+CLIENT_SECRET_FILE = 'client_secret.json'
 
 engine = create_engine('sqlite:///game_catalog.db')
 Base.metadata.bind = engine
@@ -17,27 +27,6 @@ session = DBSession()
 # for use in navigation menus
 GENRES = session.query(Genre).order_by(Genre.name)
 PLATFORMS = session.query(Platform).order_by(Platform.name)
-
-
-@app.route('/')
-def index():
-    # Get most recent items from the database
-    # to display on front page
-    recentItems = session.query(Item).order_by(desc(Item.id)).limit(5)
-    if login_session:
-        return render_template(
-            'index.html',
-            items=recentItems,
-            genres=GENRES.all(),
-            platforms=PLATFORMS.all()
-        )
-    else:
-        return render_template(
-            'indexPublic.html',
-            items=recentItems,
-            genres=GENRES.all(),
-            platforms=PLATFORMS.all()
-        )
 
 
 @app.route('/login')
@@ -59,9 +48,93 @@ def login():
     )
 
 
+@app.route('/gsignin', methods=["POST"])
+def gSignIn():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
+    auth_code = request.data
+    credentials = client.credentials_from_clientsecrets_and_code(
+        CLIENT_SECRET_FILE,
+        ['profile', 'email'],
+        auth_code
+    )
+
+    # Get profile info from ID token
+    # userid = credentials.id_token['sub']
+
+    # Get user info
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    data = json.loads(answer.text)
+
+    login_session['username'] = data['name']
+    login_session['email'] = data['email']
+    login_session['picture'] = data['picture']
+
+    user_id = getUserId(login_session['email'])
+    if not user_id:
+        user_id = createUser(login_session)
+    login_session['user_id'] = user_id
+
+    print login_session['user_id']
+    print login_session['username']
+    print login_session['email']
+    print login_session['picture']
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += '" style="width: 100px; height: 100px; border-radius: 100%;">'
+
+    return output
+
+
+@app.route('/gsignout')
+def gSignOut():
+    del login_session['user_id']
+    del login_session['username']
+    del login_session['email']
+    del login_session['picture']
+
+    response = make_response(
+        json.dumps('Successfully disconnected.'), 200
+    )
+    response.headers['Content-Type'] = 'application/json'
+
+    return redirect(url_for('index'))
+
+
+@app.route('/')
+def index():
+    # Get most recent items from the database
+    # to display on front page
+    recentItems = session.query(Item).order_by(desc(Item.id)).limit(5)
+    if 'username' not in login_session:
+        return render_template(
+            'indexPublic.html',
+            items=recentItems,
+            genres=GENRES.all(),
+            platforms=PLATFORMS.all()
+        )
+    else:
+        return render_template(
+            'index.html',
+            items=recentItems,
+            genres=GENRES.all(),
+            platforms=PLATFORMS.all()
+        )
+
+
 @app.route('/item/new', methods=["POST", "GET"])
 def newItem():
-    if login_session:
+    if 'username' not in login_session:
         return redirect(url_for('login'))
     if request.method == 'POST':
         newItem = Item(
@@ -70,7 +143,7 @@ def newItem():
             picture=request.form['picture'],
             genre_id=request.form['genre'],
             platform_id=request.form['platform'],
-            user_id=1
+            user_id=login_session['user_id']
         )
         session.add(newItem)
         session.commit()
@@ -86,13 +159,23 @@ def newItem():
 @app.route('/item/<int:item_id>')
 def showItem(item_id):
     item = session.query(Item).get(item_id)
-    return render_template(
-        'item.html',
-        item=item,
-        title=item.title,
-        genres=GENRES.all(),
-        platforms=PLATFORMS.all()
-    )
+    owner = item.user_id
+    if 'user_id' in login_session and login_session['user_id'] is owner:
+        return render_template(
+            'item.html',
+            item=item,
+            title=item.title,
+            genres=GENRES.all(),
+            platforms=PLATFORMS.all()
+        )
+    else:
+        return render_template(
+            'itemPublic.html',
+            item=item,
+            title=item.title,
+            genres=GENRES.all(),
+            platforms=PLATFORMS.all()
+        )
 
 
 @app.route('/recents_JSON')
@@ -178,4 +261,28 @@ def listByPlatform(platform_id):
         platforms=PLATFORMS.all()
     )
 
-# TODO: Add views for users
+
+def getUserId(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        return None
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def createUser(login_session):
+    newUser = User(
+        username=login_session['username'],
+        email=login_session['email'],
+        picture=login_session['picture'],
+        role_id=3
+    )
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
